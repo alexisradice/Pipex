0c23157f0d189ed6de830dc8cf9514683227b146
Alexis R <64582375+alexisradice@users.noreply.github.com>
2022-09-25 20:25:43 +0200

    update bonus

--- bonus/pipex_bonus.h
+++ bonus/pipex_bonus.h
@@ -6,7 +6,7 @@
 /*   By: aradice <aradice@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 03:11:32 by aradice           #+#    #+#             */
-/*   Updated: 2022/09/23 23:18:26 by aradice          ###   ########.fr       */
+/*   Updated: 2022/09/25 18:46:02 by aradice          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,21 @@
 
 typedef struct s_pipex
 {
-	pid_t	pid1;
-	pid_t	pid2;
+	pid_t	pid;
 	int		infile;
 	int		outfile;
 	char	**command;
 	char	**paths;
 	char	*path;
 	char	*command_path;
-	int		pipefd[2];
+	int		*pipefd;
+	int		index_child;
 }				t_pipex;
 
 void	ft_error(char *error);
 char	**ft_free_all(char **tab);
 void	ft_open_files(t_pipex *data, char *infile, char *outfile);
-void	ft_child1(t_pipex *data, char **argv, char **envp);
-void	ft_child2(t_pipex *data, char **argv, char **envp);
+void	ft_childs(t_pipex *data, int argc, char **argv, char **envp);
 int		main(int argc, char **argv, char **envp);
 char	*ft_access(t_pipex *data);
 char	*ft_path(t_pipex *data, char **envp);
